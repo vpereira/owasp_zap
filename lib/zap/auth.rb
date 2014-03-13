8b8c57be598307c3646b3bb1dc0be9c5d0d92bb6
Victor Pereira <vpereirabr@gmail.com>
2014-03-13 15:33:50 +0100

    updated auth module

--- lib/zap/auth.rb
+++ lib/zap/auth.rb
@@ -1,7 +1,6 @@
 module Zap
     class Auth
-        attr_accessor :login_url, :login_data, :loggedin_indicator, :logout_url, :logout_data, :loggedout_indicator
-        def initialize(params = {}
+        def initialize(params = {})
             @ctx = params[:context] || 1 #default context is the1
             @base = params[:base] || "http://127.0.0.1:8080/JSON"
         end
@@ -10,13 +9,14 @@ module Zap
             RestClient::get "#{@base}/auth/view/loginUrl/?zapapiformat=JSON&contextId=#{@ctx}"
         end
         
-        # params
+        # params:
+        # args a hash with the following keys -> values
         # url: url including http:// 
         # post_data: an already encoded string like "email%3Dfoo%2540example.org%26passwd%3Dfoobar" 
         # TODO: offer a way to encode it, giving a hash?
-        def login_url=(url,post_data)
+        def set_login_url(args)
             url = Addressable::URI.parse "#{@base}/auth/action/setLoginUrl/"
-            url.query_values = {:zapapiformat=>"JSON",:url=>@target,:postData=@post_data,:contextID=>@ctx}
+            url.query_values = {:zapapiformat=>"JSON",:url=>args[:url],:postData=>args[:post_data],:contextId=>@ctx}
             RestClient::get url.normalize.to_str
         end
     end
