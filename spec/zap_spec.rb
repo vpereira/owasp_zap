7dfab027b84f3c6021e3f348f823888f2283d9a5
Victor Pereira <vpereirabr@gmail.com>
2014-03-17 12:02:16 +0100

    extending test suite

--- spec/zap_spec.rb
+++ spec/zap_spec.rb
@@ -23,6 +23,13 @@ describe ZapV2 do
         @zap.respond_to? :base
     end
 
+    it "should have method start" do
+        @zap.respond_to? :start
+    end
+
+    it "should have a method shutdown" do
+        @zap.respond_to? :shutdown
+    end
     it "base shouldnt be nil" do
         @zap.base.wont_be :nil?
     end
@@ -39,3 +46,16 @@ describe "changing default params" do
     end
 end
 
+describe "method shutdown" do
+    before do
+        @h = Zap::ZapV2.new :target=>"http://127.0.0.1"
+        stub_request(:get, "http://127.0.0.1:8080/JSON/core/action/shutdown/").with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).to_return(:status => 200, :body => "{\"Result\":\"OK\"}" , :headers => {})
+
+
+
+    end
+    it "should receive a json as answer" do
+         @h.shutdown.wont_be :nil?
+    end
+
+end
