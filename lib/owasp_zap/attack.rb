94bbe952716b3d3e85e8df54f6046a6e9baeb494
Logan Carmody <ljc27@duke.edu>
2015-07-02 14:41:03 -0700

    adding important methods for scanning and spidering

--- lib/owasp_zap/attack.rb
+++ lib/owasp_zap/attack.rb
@@ -13,6 +13,12 @@ module OwaspZap
             RestClient::get url.normalize.to_str
         end
 
+        def scan_as_user(context_id, user_id)
+            url = Addressable::URI.parse("#{@base}/JSON/ascan/action/scanAsUser/")
+            url.query_values = {:zapapiformat=>"JSON",:url=>@target, contextId: context_id, userId: user_id}
+            RestClient::get url.normalize.to_str
+        end
+
         def status
             ret = JSON.parse(RestClient::get("#{@base}/JSON/ascan/view/status/?zapapiformat=JSON"))
             if ret.has_key? "status"
