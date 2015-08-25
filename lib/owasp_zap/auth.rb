d1f91c04f79c7cf3d634bd18f12679e89cc493a4
Logan Carmody <ljc27@duke.edu>
2015-08-25 13:37:19 -0700

    debugging message

--- lib/owasp_zap/auth.rb
+++ lib/owasp_zap/auth.rb
@@ -41,6 +41,7 @@ module OwaspZap
         def import_context(context, set_as_context=true)
           response = JSON.parse(set_query "#{@base}/json/context/action/importContext/", contextFile: context)
           @ctx = response[:contextId] if set_as_context
+          puts response[:contextId]
           return response[:contextId]
         end
 
